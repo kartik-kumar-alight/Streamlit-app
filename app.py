ba31725ce418f918f81bd41e57e10007d0c558ac
Kartik Kumar <kartik.kumar.2@alight.com>
2024-02-09 12:15:08 +0530

    name_change

--- app.py
+++ app.py
@@ -1,24 +1,55 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import pickle
 import numpy as np
-from prediction import predict
 
+model= joblib.load("titaniv_v0.sav")
 
-st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into \ setosa, versicolor, virginica')
+def main():
+    """ main() contains all UI structure elements; getting and storing user data can be done within it"""
+    st.title("Titanic Survival Prediction")                                                                             
+    st.write("""## Would you have survived From Titanic Disaster?""")                                                   
 
-st.header("Plant Features")
-col1, col2 = st.columns(2)
-with col1:
-    st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
-with col2:
-    st.text("Pepal characteristics")
-    petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+    st.title("-----          Check Your Survival Chances          -----")
 
+    ## Framing UI Structure
+    age = st.slider("Enter Age :", 1, 75, 30)                                                                  
 
-if st.button("Predict type of Iris"):
-    result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-    st.text(result[0])
+    fare = st.slider("Fare (in 1912 $) :", 15, 500, 40)                                                        
+
+    Parch = st.selectbox("How many Parents or children are travelling with you?", [0, 1, 2, 3, 4, 5, 6, 7, 8]) 
+
+    sex = st.selectbox("Select Gender:", ["Male","Female"])                         
+    if (sex == "Male"):                                                            
+        Sex=0
+    else:
+        Sex=1
+
+    Pclass= st.selectbox("Select Passenger-Class:",[1,2,3])                        
+
+    Embarked_C,Embarked_Q,Embarked_S=0,0,0                 
+    if boarded_location == "Queenstown":
+        Embarked_Q=1
+    elif boarded_location == "Southampton":
+        Embarked_S=1
+    else:
+        Embarked_C=1
+
+    data={"Age":age,"Fare":fare,"Parch":Parch,"Sex":Sex,"Pclass":Pclass,"Embarked_Q":Embarked_Q,"Embarked_S":Embarked_S,"Embarked_C":Embarked_C}
+
+    df=pd.DataFrame(data,index=[0])     
+    return df
+
+data=main()                           
+
+if st.button("Predict"):                                                                
+    result = model.predict(data)                                                        
+    proba=model.predict_proba(data)                                                    
+
+    if result[0] == 1:
+        st.write("***congratulation !!!....*** **You probably would Survive!**")
+        st.write("**Survival Probability Chances :** 'NO': {}%  'YES': {}% ".format(round((proba[0,0])*100,2),round((proba[0,1])*100,2)))
+    else:
+        st.write("***Better Luck Next time !!!!...*** **you're probably Die!'**")
+        st.image(r"Rip.jfif")
+        st.write("**Survival Probability Chances :** 'NO': {}%  'YES': {}% ".format(round((proba[0,0])*100,2),round((proba[0,1])*100,2)))
--- model.py
+++ model.py
@@ -1,29 +1,45 @@
-# Read original dataset
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
-iris_df = pd.read_csv("iris.csv")
-# iris_df.sample(frac=1, random_state=seed)
-# print(iris_df.head(10))
-# print(iris_df.columns)
-iris_df = iris_df.fillna(0)
-# selecting features and target data
-X = iris_df[['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']]
-y = iris_df[['F_Category']]
-# split data into train and test sets
-# 70% training and 30% test
-X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.3, stratify=y)
-# create an instance of the random forest classifier
-clf = RandomForestClassifier(n_estimators=100)
-# train the classifier on the training data
-clf.fit(X_train, y_train)
-# predict on the test set
-y_pred = clf.predict(X_test)
-# calculate accuracy
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}") # Accuracy: 0.91
 
-joblib.dump(clf, "rf_model.sav")
+df = pd.read_excel("titanic.xlsx")
+# print(df.head(10))
+
+def sex_encoder(data):
+    mapper={"male":0,"female":1}  
+    data["Sex"]=data["Sex"].replace(mapper) 
+    return data
+
+df = sex_encoder(df)
+
+def embarked_encoder(data):
+    df=pd.get_dummies(data=data["Embarked"],prefix='Embarked')
+    data=pd.concat([data,df],axis=1) 
+    data.drop(["Embarked"],axis=1,inplace=True)
+    return data
+    
+df = embarked_encoder(df)
+
+# print(df.columns)
+X = df[['Pclass', 'Sex', 'Age', 'Parch', 'Fare', 'Embarked_C', 'Embarked_Q', 'Embarked_S']]
+y = df[['Survived']]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+lr_pred=lr.predict(X_test)
+lr_proba=lr.predict_proba(X_test) 
+accuracy = accuracy_score(y_test, lr_pred)
+print(f"Accuracy: {accuracy}")
+
+
+
+# ## Saving Model
+
+joblib.dump(lr, "titaniv_v0.sav")
+
